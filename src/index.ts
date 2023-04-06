a6d55d802f1a15ca46d2c90b30b616bf19d0d454
bigbeno37 <contact@benosullivan.com>
2023-04-06 18:25:05 +1000

    Added test Tweet types; root route now renders tweets

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,75 @@
 import express from 'express';
+import {formatISO, compareDesc} from 'date-fns';
 const app = express();
+
+// TODO: Remove hardcoded test data and generate these types from Zod schemas
+type User = {
+    username: string;
+    createdAt: Date;
+    password: string;
+    salt: string;
+};
+
+const USERS: User[] = [
+    {
+        username: 'user1',
+        createdAt: new Date(),
+        password: 'password1',
+        salt: 'salt1',
+    }
+];
+
+type UserFollow = {
+    username: string;
+    followingUsername: string;
+}
+
+const USER_FOLLOWS: UserFollow[] = [];
+
+type Tweet = {
+    id: string;
+    username: string;
+    createdAt: Date;
+    text: string;
+};
+
+const tweet = (username: string, text: string): Tweet => {
+    return {
+        id: '1',
+        username,
+        createdAt: new Date(),
+        text,
+    };
+}
+
+const TWEETS: Tweet[] = [
+    tweet('user1', 'Wow, this is an awesome tweet!'),
+    tweet('user1', 'Cool story bro'),
+];
+
+type TweetLike = {
+    tweetId: string;
+    username: string;
+}
+
+const TWEET_LIKES: TweetLike[] = [];
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static('static'));
 
+app.use((req, res, next) => {
+    // Add formatISO as a local for EJS templates
+    res.locals['formatISO'] = formatISO;
+    next();
+});
+
+// A GET route for root, that returns the string Hello World!
 app.get('/', (req, res) => {
-    res.render('index');
+    const sortedTweets = TWEETS.sort((a, b) => compareDesc(a.createdAt, b.createdAt));
+
+    res.render('index', { tweets: sortedTweets });
 });
 
 // Run the server on port 3000
