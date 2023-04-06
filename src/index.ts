968f490203cd8d27d2e4793a1218fbe630843460
bigbeno37 <contact@benosullivan.com>
2023-04-06 18:07:21 +1000

    Added basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import express from 'express';
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Run the server on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
