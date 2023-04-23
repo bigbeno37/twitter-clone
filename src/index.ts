31dbe9d53e77ec817cc9d23f3d681f2461ecb331
bigbeno37 <contact@benosullivan.com>
2023-04-24 01:30:26 +1000

    Added ability to register account (stored in-memory for now)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { compareDesc, format } from 'date-fns';
+import * as argon2 from "argon2";
+import {nanoid} from "nanoid";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 // TODO: Remove hardcoded test data and generate these types from Zod schemas
@@ -7,17 +11,16 @@ type User = {
     username: string;
     createdAt: Date;
     password: string;
-    salt: string;
 };
 
-const USERS: User[] = [
-    {
-        username: 'user1',
-        createdAt: new Date(),
-        password: 'password1',
-        salt: 'salt1',
-    }
-];
+const USERS: User[] = [];
+
+type AuthToken = {
+    token: string;
+    username: string;
+};
+
+const AUTH_TOKENS: AuthToken[] = [];
 
 type UserFollow = {
     username: string;
@@ -60,6 +63,7 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const getSortedTweets = () => {
     return TWEETS.sort((a, b) => compareDesc(a.createdAt, b.createdAt));
@@ -68,6 +72,24 @@ const getSortedTweets = () => {
 app.use((req, res, next) => {
     // Add formatISO as a local for EJS templates
     res.locals['format'] = format;
+    next();
+});
+
+app.use((req, res, next) => {
+    // Middleware that checks AUTH_TOKENS for a valid token from the authToken cookie in the request
+    const authToken = req.cookies?.authToken;
+
+    console.log('auth token', authToken);
+
+    if (authToken) {
+        const username = AUTH_TOKENS.find(u => u.token === authToken)?.username;
+        console.log('username', username);
+
+        if (username) {
+            res.locals['username'] = username;
+        }
+    }
+
     next();
 });
 
@@ -85,6 +107,41 @@ app.post('/tweet', (req, res) => {
     TWEETS.push({ id: '5', username: 'user1', createdAt: new Date(), text: tweet });
 
     res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        // Validate user input
+        const { username, password, confirmPassword } = req.body;
+        if (!username || username.trim().length === 0 || !password || !confirmPassword) {
+            return res.render('register', { errorMessage: 'Please provide a username, password, and confirmation password.' });
+        }
+        if (password !== confirmPassword) {
+            return res.render('register', { errorMessage: 'Passwords do not match.' });
+        }
+
+        // Hash the password using argon2
+        const hashedPassword = await argon2.hash(password);
+
+        // Store the user in your database
+        USERS.push({ username, createdAt: new Date(), password: hashedPassword });
+
+        const token = nanoid();
+
+        AUTH_TOKENS.push({ token, username });
+
+        // Set the auth token in a cookie
+        res.cookie('authToken', token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.render('register', { errorMessage: 'An error occurred while processing your request. Please try again later.' });
+    }
 });
 
 // Run the server on port 3000
