7a1db9ef0ae96501a6f622a9820941ebe7f74627
bigbeno37 <contact@benosullivan.com>
2023-05-26 21:54:23 +1000

    App now uses postgres DB rather than in-memory

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import "@total-typescript/ts-reset";
+
 import express, {RequestHandler} from 'express';
 import {add, compareDesc, format, isPast} from 'date-fns';
 import * as argon2 from "argon2";
 import {nanoid} from "nanoid";
 import cookieParser from "cookie-parser";
 import type {Response} from "express";
-
-const app = express();
-
-// TODO: Remove hardcoded test data and generate these types from Zod schemas
-type User = {
-    username: string;
-    createdAt: Date;
-    passwordHash: string;
-};
-
-const USERS: User[] = [];
-
-type UserSession = {
-    token: string;
-    username: string;
-    expiry: Date;
-    sessionData: Record<string, any>;
-};
-
-const USER_SESSIONS: UserSession[] = [];
-
-type UserFollow = {
-    username: string;
-    followingUsername: string;
-}
-
-const USER_FOLLOWS: UserFollow[] = [];
-
-type Tweet = {
-    id: string;
-    username: string;
-    createdAt: Date;
-    text: string;
-};
-
-const tweet = (username: string, text: string): Tweet => {
-    return {
-        id: '1',
-        username,
-        createdAt: new Date(),
-        text,
-    };
-}
-
-const TWEETS: Tweet[] = [
-    tweet('user1', 'Wow, this is an awesome tweet!'),
-    tweet('user1', 'Cool story bro'),
-];
-
-type TweetLike = {
-    tweetId: string;
-    username: string;
-}
-
-const TWEET_LIKES: TweetLike[] = [];
+import {Client} from 'pg';
 
 const THIRTY_DAYS = 1000 * 60 * 60 * 24 * 30;
 
@@ -74,164 +25,200 @@ declare global {
     }
 }
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
+// TODO: Check if I can use top level await. This works for now.
+(async () => {
+    const client = new Client();
+    await client.connect();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use('/static', express.static('static'));
+    const app = express();
 
-const getSortedTweets = () => {
-    return TWEETS.sort((a, b) => compareDesc(a.createdAt, b.createdAt));
-};
+    // Set EJS as the view engine
+    app.set('view engine', 'ejs');
 
-const requestIsAuthenticated = (req: express.Request) => {
-    return !!req.session?.username;
-};
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use('/static', express.static('static'));
 
-app.use((req, res, next) => {
-    res.locals['format'] = format;
+    const requestIsAuthenticated = (req: express.Request) => {
+        return !!req.session?.username;
+    };
 
-    next();
-});
+    app.use((req, res, next) => {
+        res.locals['format'] = format;
+
+        next();
+    });
 
 // ============= PUBLIC ROUTES =============
 
-const clearAuthToken = (res: Response) => {
-    res.clearCookie('authToken');
-};
+    const clearAuthToken = (res: Response) => {
+        res.clearCookie('authToken');
+    };
 
-app.use((req, res, next) => {
-    const authToken = req.cookies?.authToken;
-    if (!authToken) {
-        return next();
-    }
+    app.use((req, res, next) => {
+        const authToken = req.cookies?.authToken;
+        if (!authToken) {
+            return next();
+        }
 
-    const session = USER_SESSIONS.find(u => u.token === authToken);
-    if (!session) {
-        clearAuthToken(res);
-        return next();
-    }
+        client
+            .query<{ username: string, sessionData: Record<string, any>, expiry: Date }>('SELECT username, sessionData, expiry FROM AccountSession WHERE token = $1', [authToken])
+            .then(result => {
+                const session = result.rows[0] ?? null;
+                if (!session) {
+                    clearAuthToken(res);
+                    return next();
+                }
 
-    const expiry = session.expiry;
-    if (isPast(expiry)) {
-        clearAuthToken(res);
-        return next();
-    }
+                const expiry = session.expiry;
+                if (isPast(expiry)) {
+                    clearAuthToken(res);
+                    return next();
+                }
 
-    req.session = session;
-    res.locals['session'] = req.session;
+                req.session = session;
+                res.locals['session'] = req.session;
 
-    next();
-});
+                next();
+            })
+            .catch(console.error);
+    });
 
-app.get('/', (req, res) => {
-    res.render('index', { tweets: getSortedTweets() });
-});
+    const getSortedTweets = () => {
+        return client
+            .query<{ username: string, text: string, createdat: Date }>('SELECT username, text, createdAt FROM Tweet ORDER BY createdAt DESC')
+            .then(result => result.rows);
+    };
+
+    app.get('/', (req, res) => {
+        getSortedTweets().then(tweets => {
+            res.render('index', { tweets });
+        });
+    });
 
 // ================== AUTHENTICATED ROUTES ==================
 
-const isAuthenticated: RequestHandler = (req, res, next) => {
-    // Check to see if session exists
-    if (!requestIsAuthenticated(req)) {
-        return res.redirect('/login');
-    }
+    const isAuthenticated: RequestHandler = (req, res, next) => {
+        // Check to see if session exists
+        if (!requestIsAuthenticated(req)) {
+            return res.redirect('/login');
+        }
 
-    // Otherwise, proceed as normal
-    next();
-};
+        // Otherwise, proceed as normal
+        next();
+    };
 
-app.post('/tweet', isAuthenticated, (req, res) => {
-    const tweet = req.body?.tweet;
+    app.post('/tweet', isAuthenticated, (req, res) => {
+        const tweet = req.body?.tweet;
 
-    if (!tweet) {
-        return res.render('index', { tweets: getSortedTweets(), error: 'Tweet cannot be empty' });
-    }
+        if (!tweet) {
+            return getSortedTweets().then(tweets => {
+                res.render('index', { tweets, error: 'Tweet cannot be empty' });
+            });
+        }
 
-    TWEETS.push({ id: TWEETS.length+1+'', username: req.session?.username!, createdAt: new Date(), text: tweet });
+        // TODO: Handle errors here...somehow
+        client.query('INSERT INTO Tweet (username, text) VALUES ($1, $2)', [req.session!.username, tweet]);
 
-    res.redirect('/');
-});
+        res.redirect('/');
+    });
 
-app.get('/logout', isAuthenticated, (req, res) => {
-    res.clearCookie('authToken');
-    res.redirect('/');
-});
+    app.get('/logout', isAuthenticated, (req, res) => {
+        res.clearCookie('authToken');
+        res.redirect('/');
+    });
 
 // ================== UNAUTHENTICATED-ONLY ROUTES ==================
 
-const isNotAuthenticated: RequestHandler = (req, res, next) => {
-    // Check to see if session exists
-    if (requestIsAuthenticated(req)) {
-        return res.redirect('/');
-    }
-
-    // Otherwise, proceed as normal
-    next();
-};
-
-app.get('/register', isNotAuthenticated, (req, res) => {
-    res.render('register');
-});
-
-app.post('/register', isNotAuthenticated, async (req, res) => {
-    try {
-        // Validate user input
-        const { username, password, confirmPassword } = req.body;
-        if (!username || username.trim().length === 0 || !password || !confirmPassword) {
-            return res.render('register', { errorMessage: 'Please provide a username, password, and confirmation password.' });
-        }
-        if (password !== confirmPassword) {
-            return res.render('register', { errorMessage: 'Passwords do not match.' });
+    const isNotAuthenticated: RequestHandler = (req, res, next) => {
+        // Check to see if session exists
+        if (requestIsAuthenticated(req)) {
+            return res.redirect('/');
         }
 
-        // Hash the password using argon2
-        const hashedPassword = await argon2.hash(password);
+        // Otherwise, proceed as normal
+        next();
+    };
 
-        // Store the user in your database
-        USERS.push({ username, createdAt: new Date(), passwordHash: hashedPassword });
+    app.get('/register', isNotAuthenticated, (req, res) => {
+        res.render('register');
+    });
 
-        const token = nanoid();
+    app.post('/register', isNotAuthenticated, async (req, res) => {
+        try {
+            // Validate user input
+            const { username, password, confirmPassword } = req.body;
+            if (!username || username.trim().length === 0 || !password || !confirmPassword) {
+                return res.render('register', { errorMessage: 'Please provide a username, password, and confirmation password.' });
+            }
+            if (password !== confirmPassword) {
+                return res.render('register', { errorMessage: 'Passwords do not match.' });
+            }
 
-        USER_SESSIONS.push({ token, username, expiry: add(new Date(), { days: 30 }), sessionData: {} });
+            // Hash the password using argon2
+            const hashedPassword = await argon2.hash(password);
 
-        // Set the auth token in a cookie
-        res.cookie('authToken', token, { httpOnly: true, maxAge: THIRTY_DAYS });
+            // Store the user in your database
+            await client.query('INSERT INTO Account (username, passwordHash) VALUES ($1, $2)', [username, hashedPassword]);
 
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        res.render('register', { errorMessage: 'An error occurred while processing your request. Please try again later.' });
-    }
-});
+            const token = nanoid();
 
-app.get('/login', isNotAuthenticated, (req, res) => {
-    return res.render('login');
-});
+            await client.query(
+                'INSERT INTO AccountSession (token, username, expiry, sessionData) VALUES ($1, $2, $3, $4)',
+                [
+                    token,
+                    username,
+                    add(new Date(), { days: 30 }),
+                    {}
+                ]
+            );
 
-app.post('/login', isNotAuthenticated, async (req, res) => {
-    const { username, password, remember } = req.body;
+            // Set the auth token in a cookie
+            res.cookie('authToken', token, { httpOnly: true, maxAge: THIRTY_DAYS });
 
-    const user = USERS.find(u => u.username === username);
-    const passwordMatchesHash = user && (await argon2.verify(user.passwordHash, password));
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.render('register', { errorMessage: 'An error occurred while processing your request. Please try again later.' });
+        }
+    });
 
-    if (user && passwordMatchesHash) {
-        const token = nanoid();
+    app.get('/login', isNotAuthenticated, (req, res) => {
+        return res.render('login');
+    });
 
-        USER_SESSIONS.push({ token, username, expiry: add(new Date(), { days: 30 }), sessionData: {} });
+    app.post('/login', isNotAuthenticated, async (req, res) => {
+        const { username, password, remember } = req.body;
 
-        // Set the auth token in a cookie
-        res.cookie('authToken', token, { httpOnly: true, maxAge: remember ? THIRTY_DAYS : undefined });
+        const user = await client.query<{ passwordHash: string }>('SELECT passwordHash FROM Account WHERE username = $1', [username])
+            .then(result => result.rows[0] ?? null);
+        const passwordMatchesHash = user && (await argon2.verify(user.passwordHash, password));
 
-        // If the authentication succeeds, redirect the user to the homepage
-        return res.redirect('/');
-    } else {
-        // If the authentication fails, render the login template with an error message
-        return res.render('login', { errorMessage: 'Invalid username or password' });
-    }
-});
+        if (user && passwordMatchesHash) {
+            const token = nanoid();
 
-// Run the server on port 3000
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+            await client.query(
+                'INSERT INTO AccountSession (token, username, expiry, sessionData) VALUES ($1, $2, $3, $4)',
+                [
+                    token,
+                    username,
+                    add(new Date(), { days: 30 }),
+                    {}
+                ]
+            );
+
+            // Set the auth token in a cookie
+            res.cookie('authToken', token, { httpOnly: true, maxAge: remember ? THIRTY_DAYS : undefined });
+
+            // If the authentication succeeds, redirect the user to the homepage
+            return res.redirect('/');
+        } else {
+            // If the authentication fails, render the login template with an error message
+            return res.render('login', { errorMessage: 'Invalid username or password' });
+        }
+    });
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})();
